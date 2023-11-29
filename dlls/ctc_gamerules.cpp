ff5f227c5b8e935e6c0fa7f1e42d198b7470810f
Doug Arcuri <xsurrealx@hotmail.com>
2023-11-29 18:22:58 -0500

    Allow trigger_hurt to inflict damge in ctc

--- dlls/ctc_gamerules.cpp
+++ dlls/ctc_gamerules.cpp
@@ -379,6 +379,9 @@ int CHalfLifeCaptureTheChumtoad::DeadPlayerWeapons( CBasePlayer *pPlayer )
 
 BOOL CHalfLifeCaptureTheChumtoad::FPlayerCanTakeDamage( CBasePlayer *pPlayer, CBaseEntity *pAttacker )
 {
+	if (strcmp(STRING(pAttacker->pev->classname), "trigger_hurt") == 0)
+		return TRUE;
+
 	return pPlayer->m_iHoldingChumtoad ? TRUE : FALSE;
 }
 
