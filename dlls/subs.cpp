a6166f4738f9458628331583250c2a447aa46e41
Doug Arcuri <xsurrealx@hotmail.com>
2025-12-30 09:18:07 -0500

    Add coop to entities

--- dlls/subs.cpp
+++ dlls/subs.cpp
@@ -68,6 +68,7 @@ private:
 // These are the new entry points to entities. 
 LINK_ENTITY_TO_CLASS(info_player_deathmatch,CBaseDMStart);
 LINK_ENTITY_TO_CLASS(info_player_start,CPointEntity);
+LINK_ENTITY_TO_CLASS(info_player_coop,CPointEntity);
 LINK_ENTITY_TO_CLASS(info_landmark,CPointEntity);
 
 void CBaseDMStart::KeyValue( KeyValueData *pkvd )
