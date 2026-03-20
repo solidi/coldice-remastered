1ed1ebddf392af042e04addc4b8df02ec128708b
solidi <xsurrealx@hotmail.com>
2026-03-20 19:42:26 -0400

    Add thirdperson support for busters, fix egon forcegrab

--- dlls/busters_gamerules.h
+++ dlls/busters_gamerules.h
@@ -37,10 +37,14 @@ public:
 	virtual BOOL FPlayerCanTakeDamage( CBasePlayer *pPlayer, CBaseEntity *pAttacker );
 	virtual BOOL IsTeamplay( void );
 	virtual BOOL CanHaveNamedItem( CBasePlayer *pPlayer, const char *pszItemName );
-
+	virtual BOOL FShouldSwitchWeapon( CBasePlayer *pPlayer, CBasePlayerItem *pWeapon );
+	virtual BOOL CanRandomizeWeapon( const char *name );
+	virtual BOOL CanHavePlayerAmmo( CBasePlayer *pPlayer, CBasePlayerAmmo *pAmmo );
+	virtual BOOL IsAllowedToDropWeapon( CBasePlayer *pPlayer );
 	void SetPlayerModel( CBasePlayer* pPlayer );
 
 protected:
 	float m_flEgonBustingCheckTime = -1.0f;
+	float m_flCheckForWeapons = -1.0f;
 	void CheckForEgons( void );
 };
