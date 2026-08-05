e18dee57ab55f4faeffca730a1bf70a573fedbca
solidi <xsurrealx@hotmail.com>
2026-08-05 13:48:32 -0400

    Provide the initial nuke package code

--- dlls/satchel.cpp
+++ dlls/satchel.cpp
@@ -117,11 +117,56 @@ void CSatchelCharge :: Spawn( void )
 
 void CSatchelCharge::SatchelSlide( CBaseEntity *pOther )
 {
-	entvars_t	*pevOther = pOther->pev;
-
 	// don't hit the guy that launched this grenade
 	if ( pOther->edict() == pev->owner )
 		return;
+
+#ifndef CLIENT_DLL
+	if ( FBitSet( pev->spawnflags, SF_SATCHEL_NUKE_PACKAGE ) )
+	{
+		CBasePlayer *pOwner = NULL;
+		if (pev->owner)
+		{
+			CBaseEntity *pOwnerEnt = CBaseEntity::Instance( pev->owner );
+			if (pOwnerEnt && pOwnerEnt->IsPlayer())
+				pOwner = (CBasePlayer *)pOwnerEnt;
+		}
+
+		Vector vecNormal = Vector(0, 0, 1);
+		if (pOther && pOther->IsBSPModel())
+		{
+			Vector vecVelDir = pev->velocity;
+			float flVelLen = vecVelDir.Length();
+			if (flVelLen > 1.0f)
+				vecVelDir = vecVelDir / flVelLen;
+			else
+				vecVelDir = Vector(0, 0, -1);
+
+			TraceResult trAttach;
+			UTIL_TraceLine( pev->origin + vecVelDir * 4, pev->origin - vecVelDir * 24, ignore_monsters, edict(), &trAttach );
+			if (trAttach.flFraction < 1.0f)
+				vecNormal = trAttach.vecPlaneNormal;
+		}
+		else if (pOther)
+		{
+			vecNormal = (pev->origin - pOther->Center()).Normalize();
+		}
+
+		float flNormalLenSq = vecNormal.x * vecNormal.x + vecNormal.y * vecNormal.y + vecNormal.z * vecNormal.z;
+		if (flNormalLenSq < 0.01f)
+			vecNormal = Vector(0, 0, 1);
+
+		if (pOwner && DeployProxMineAt( pOwner, pev->origin + vecNormal * 8, vecNormal, TRUE ))
+		{
+			BounceSound();
+			UTIL_Remove( this );
+			return;
+		}
+
+		UTIL_Remove( this );
+		return;
+	}
+#endif
 
 	// pev->avelocity = Vector (300, 300, 300);
 	pev->gravity = 1;// normal gravity now
@@ -172,7 +217,7 @@ void CSatchelCharge :: SatchelThink( void )
 		pev->velocity.z -= 8;
 	}
 
-	if ( !m_transformed && pev->velocity.Length() < 150 ) {
+	if ( !FBitSet( pev->spawnflags, SF_SATCHEL_NUKE_PACKAGE ) && !m_transformed && pev->velocity.Length() < 150 ) {
 		MESSAGE_BEGIN( MSG_PVS, SVC_TEMPENTITY, pev->origin );
 			WRITE_BYTE( TE_SMOKE );
 			WRITE_COORD( pev->origin.x );
@@ -646,7 +691,6 @@ void CSatchel::Reload( void )
 	if ( m_pPlayer->m_flNextAttack > UTIL_WeaponTimeBase() )
 		return;
 
-	extern BOOL DeployProxMine( CBasePlayer *pPlayer );
 	if ( DeployProxMine( m_pPlayer ) )
 	{
 		m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType]--;
