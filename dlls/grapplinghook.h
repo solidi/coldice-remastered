f710e1a2521a29776aa8c3f731434b275323ca8f
solidi <xsurrealx@hotmail.com>
2026-08-12 20:00:02 -0400

    Add initial scorpion attack

--- dlls/grapplinghook.h
+++ dlls/grapplinghook.h
@@ -19,12 +19,24 @@ public:
 	virtual int ObjectCaps( void ) { return CBaseEntity::ObjectCaps() & ~FCAP_ACROSS_TRANSITION | FCAP_PORTAL; }
 
 private:
+	void BeginFatalPull( CBasePlayer *pVictim );
+	void UpdateFatalPull( void );
+	void RestoreFatalVictimPhysics( void );
+	void DoFatalUppercut( CBasePlayer *pOwner, CBasePlayer *pVictim );
+
 	Vector m_vPlayerHangOrigin;
 	BOOL m_fPlayerAtEnd;
 	short ropesprite;
 	BOOL m_fHookInWall;
 	BOOL m_fActiveHook;
 	Vector m_vVecDirHookMove;
+	EHANDLE m_hFatalVictim;
+	BOOL m_fFatalPull;
+	BOOL m_fVictimPhysicsCaptured;
+	int m_iVictimMoveType;
+	float m_flVictimGravity;
+	float m_flVictimFriction;
+	float m_flFatalAbortTime;
 	EHANDLE pevOwner;
 };
 
